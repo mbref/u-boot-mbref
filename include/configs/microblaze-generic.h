Xilinx/u-boot-xlnx

microblaze: Enable ubi support
Signed-off-by: Michal Simek <monstr@monstr.eu>

include/configs/microblaze-generic.h
@@ -31,6 +31,17 @@
 #define	CONFIG_MICROBLAZE	1
 #define	MICROBLAZE_V5		1
 
+/* UBI commands */
+#define	CONFIG_CMD_UBI
+#define	CONFIG_RBTREE
+#define	CONFIG_MTD_PARTITIONS
+
+/* UBIFS */
+/*
+#define	CONFIG_CMD_UBIFS
+#define	CONFIG_LZO
+*/
+
 /* uart */
 #ifdef XILINX_UARTLITE_BASEADDR
 # define CONFIG_XILINX_UARTLITE
@@ -141,7 +152,7 @@
 		(CONFIG_SYS_SDRAM_SIZE - GENERATED_GBL_DATA_SIZE)
 
 /* monitor code */
-#define	SIZE				0x40000
+#define	SIZE				0x100000
 #define	CONFIG_SYS_MONITOR_LEN		SIZE
 #define	CONFIG_SYS_MONITOR_BASE	\
 		(CONFIG_SYS_SDRAM_BASE + CONFIG_SYS_GBL_DATA_OFFSET \

mbref/u-boot-mbref

include/configs/microblaze-generic.h
@@ -31,6 +31,17 @@
 #define	CONFIG_MICROBLAZE	1
 #define	MICROBLAZE_V5		1
 
+/* UBI commands */
+#define	CONFIG_CMD_UBI
+#define	CONFIG_RBTREE
+#define	CONFIG_MTD_PARTITIONS
+
+/* UBIFS */
+/*
+#define	CONFIG_CMD_UBIFS
+#define	CONFIG_LZO
+*/
+
 /* uart */
 #ifdef XILINX_UARTLITE_BASEADDR
 # define CONFIG_XILINX_UARTLITE
@@ -143,14 +154,13 @@
 		(CONFIG_SYS_SDRAM_SIZE - GENERATED_GBL_DATA_SIZE)
 
 /* monitor code */
-#define	SIZE				0x40000
-#define	CONFIG_SYS_MONITOR_LEN		SIZE
+#define	CONFIG_SYS_MONITOR_LEN		0x40000
 #define	CONFIG_SYS_MONITOR_BASE	\
 		(CONFIG_SYS_SDRAM_BASE + CONFIG_SYS_GBL_DATA_OFFSET \
 			- CONFIG_SYS_MONITOR_LEN - GENERATED_BD_INFO_SIZE)
 #define	CONFIG_SYS_MONITOR_END \
 			(CONFIG_SYS_MONITOR_BASE + CONFIG_SYS_MONITOR_LEN)
-#define	CONFIG_SYS_MALLOC_LEN		SIZE
+#define	CONFIG_SYS_MALLOC_LEN		0x100000
 #define	CONFIG_SYS_MALLOC_BASE \
 			(CONFIG_SYS_MONITOR_BASE - CONFIG_SYS_MALLOC_LEN)
 