Xilinx/u-boot-xlnx

romfs: Add ROMFS implementation
Signed-off-by: Michal Simek <monstr@monstr.eu>

Makefile
@@ -194,8 +194,8 @@ LIBS += arch/arm/cpu/ixp/npe/libnpe.a
 endif
 LIBS += arch/$(ARCH)/lib/lib$(ARCH).a
 LIBS += fs/cramfs/libcramfs.a fs/fat/libfat.a fs/fdos/libfdos.a fs/jffs2/libjffs2.a \
-	fs/reiserfs/libreiserfs.a fs/ext2/libext2fs.a fs/yaffs2/libyaffs2.a \
-	fs/ubifs/libubifs.a
+	fs/reiserfs/libreiserfs.a fs/romfs/libromfs.a fs/ext2/libext2fs.a \
+	fs/yaffs2/libyaffs2.a fs/ubifs/libubifs.a
 LIBS += net/libnet.a
 LIBS += disk/libdisk.a
 LIBS += drivers/bios_emulator/libatibiosemu.a

mbref/u-boot-mbref

Signed-off-by: Stephan Linz <linz@li-pro.net>

Makefile
@@ -243,8 +243,8 @@ LIBS += dts/libdts.o
 endif
 LIBS += arch/$(ARCH)/lib/lib$(ARCH).o
 LIBS += fs/cramfs/libcramfs.o fs/fat/libfat.o fs/fdos/libfdos.o fs/jffs2/libjffs2.o \
-	fs/reiserfs/libreiserfs.o fs/ext2/libext2fs.o fs/yaffs2/libyaffs2.o \
-	fs/ubifs/libubifs.o
+	fs/reiserfs/libreiserfs.o fs/romfs/libromfs.a fs/ext2/libext2fs.o \
+	fs/yaffs2/libyaffs2.o fs/ubifs/libubifs.o
 LIBS += net/libnet.o
 LIBS += disk/libdisk.o
 LIBS += drivers/bios_emulator/libatibiosemu.o